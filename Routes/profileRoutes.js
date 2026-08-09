225d251119c604f5f44107da2fb355f240f60144
ben-websites <sabihuddin309@gmail.com>
2026-08-09 22:58:59 +0500

    Add profile routes for getting and updating profiles

---
+++ Routes/profileRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const {
+  getProfile,
+  updateProfile,
+} = require("../Controllers/profileController");
+
+const router = express.Router();
+
+router.get("/profile/:id", getProfile);
+
+router.put("/profile/:id", updateProfile);
+
+module.exports = router;
