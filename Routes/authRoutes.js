b418586262ded85c34e117dbccb8bfa77600e6e1
ben-websites <sabihuddin309@gmail.com>
2026-08-09 23:45:38 +0500

    Refactor authRoutes to include profile update endpoint

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const authController = require("../Controllers/authController.js");
 const Middleware = require("../Middleware/authMIddleware.js");
+const upload = require("../Middleware/uploadMiddleware.js");
 
 const authrouter = express.Router();
 
+authrouter.post(
+  "/register",
+  Middleware.registerMiddleware,
+  authController.Register
+);
 
+authrouter.post(
+  "/login",
+  Middleware.loginMiddleware,
+  authController.Login
+);
 
+authrouter.put(
+  "/profile/:id",
+  upload.single("profilePic"),
+  authController.UpdateProfile
+);
 
-authrouter.post('/register', Middleware.registerMiddleware, authController.Register)
-authrouter.post('/login', Middleware.loginMiddleware, authController.Login)
-
-module.exports = authrouter
-
-
+module.exports = authrouter;
